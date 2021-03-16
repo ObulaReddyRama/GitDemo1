58451c0f3131b75f27817e62ab5f6a57a58a737d
RAMA_OBULA_reddy <Rama_Obula_reddy@Dellteam.com>
2021-03-16 15:24:23 +0530

    csdcsdcsdcsd

--- src/com/ds/Test.java
+++ src/com/ds/Test.java
@@ -4,6 +4,12 @@ public class Test {
 	
 	public static void main(String[] args) {
 		m2("Rama");
+		m3("1",2,"3");
+	}
+
+	private static void m3(String string, int i, String string2) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	private static void m2(String string) {
