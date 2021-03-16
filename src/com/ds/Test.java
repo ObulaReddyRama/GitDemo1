46b815d5ae18235e207fd38573776961630acdcc
RAMA_OBULA_reddy <Rama_Obula_reddy@Dellteam.com>
2021-03-16 09:50:43 +0530

    added m2

--- src/com/ds/Test.java
+++ src/com/ds/Test.java
@@ -3,12 +3,14 @@ package com.ds;
 public class Test {
 	
 	public static void main(String[] args) {
-		m1("Rama",1);
+		m2("Rama",1);
 	}
 
-	private static void m1(String string, int i) {
-		System.out.println(string+""+i);
+	private static void m2(String string, int i) {
+		// TODO Auto-generated method stub
 		
 	}
+
+	
 
 }
